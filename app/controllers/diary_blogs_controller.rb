036d16c6ba3204d41cb794278fa17b41e8cccadb
EC2 Default User <nanaishlaughaway@gmail.com>
2022-05-19 13:46:50 +0000

    [add]modal

--- app/controllers/diary_blogs_controller.rb
+++ app/controllers/diary_blogs_controller.rb
@@ -1,15 +1,25 @@
 class DiaryBlogsController < ApplicationController
-  
+
   def new
     @blog = Blog.new
   end
-  
+
   def index
     @blogs = DiaryBlog.all
     @blog = DiaryBlog.new
   end
 
+  def create
+    DiaryBlog.new(blog_parameter)
+    redirect_to request.referer
+  end
+
   def show
   end
-  
+
+  private
+
+  def blog_parameter
+    params.require(:diary_blog).permit(:title, :content, :start_time)
+  end
 end
